c1484605ed62ee29e135dc8ccc81a5dcd689e0af
Никита Шидогубов <nikita.sh@gklifts.ru>
2024-09-25 08:33:41 +0300

    Изменение settings.py

--- API_for_library_management/settings.py
+++ API_for_library_management/settings.py
@@ -111,6 +111,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ]
 }
 
 # JWT настройки
